d9f4904bf701d0c6dced3c6dbd21cc8679ea0014
Elijah Paminsan <e.paminsan@gmail.com>
2019-09-18 12:44:09 -0700

    refactored components with props to useContext

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Navigation from './components/Navigation'
 import Products from './components/Products'
 import ShoppingCart from './components/ShoppingCart'
 
-// Context
+// Contexts
 import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 function App() {
   const [products] = useState(data)
@@ -19,18 +20,15 @@ function App() {
   }
 
   return (
-    <div className='App'>
-      <Navigation cart={cart} />
-
-      {/* Routes */}
-      <Route
-        exact
-        path='/'
-        render={() => <Products products={products} addItem={addItem} />}
-      />
-
-      <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
-    </div>
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={cart}>
+        <div className='App'>
+          <Navigation />
+          <Route exact path='/' component={Products} />
+          <Route path='/cart' component={ShoppingCart} />
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
   )
 }
 
